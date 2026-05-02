d57b6e9c4670d236c3010e051bf93cfd1ad4c11b
dmitrymack <dmitry.m@zimalab.com>
2026-05-02 22:59:17 +0500

    [Increment 1] Created server and added first handlers

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,19 @@
 package main
 
-func main() {}
+import (
+	"net/http"
+
+	"github.com/dmitrymack/go-url-shortener.git/internal/handler"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc(`/`, handler.SetShortUrl)
+	mux.HandleFunc(`/{id}`, handler.GetUrlById)
+
+	err := http.ListenAndServe(`:8080`, mux)
+
+	if err != nil {
+		panic(err)
+	}
+}
---
+++ internal/handler/handler.go
@@ -0,0 +1,47 @@
+package handler
+
+import (
+	"io"
+	"net/http"
+)
+
+var storage = map[string]string{
+	"abc123": "https://practicum.yandex.ru/",
+}
+
+func SetShortUrl(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Only POST requests are allowed", http.StatusBadRequest)
+		return
+	}
+
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Bad Request", http.StatusBadRequest)
+		return
+	}
+	defer r.Body.Close()
+
+	originURL := string(body)
+
+	if originURL == "" {
+		http.Error(w, "URL is required", http.StatusBadRequest)
+		return
+	}
+
+	id := "abc123"
+	shortURL := "http://" + r.Host + "/" + id + "\n"
+
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte(shortURL))
+}
+
+func GetUrlById(w http.ResponseWriter, r *http.Request) {
+	if storage[r.PathValue("id")] == "" {
+		http.Error(w, "URL not found", http.StatusBadRequest)
+		return
+	}
+
+	w.Header().Set("Location", storage[r.PathValue("id")])
+	w.WriteHeader(http.StatusTemporaryRedirect)
+}
